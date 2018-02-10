f920f7a9f1086090b858bc44e4168a7879996667
Andy Wang <it@pacificmun.org>
2018-02-10 12:04:05 -0800

    Image Test Mobile

--- js/coffee.js
+++ js/coffee.js
@@ -1,4 +1,14 @@
 function function1 ()
 {
-      document.getElementById('image1').src = "img/test2.png";
+      if (textclicked === true)
+      {
+            document.getElementById('image1').src = "img/test2.png";
+      }
+
+}
+
+function mainP ()
+{
+      textclicked = true;
+      document.getElementById('text1').innerHTML = "How do I make coffee again??(click an object)";
 }
